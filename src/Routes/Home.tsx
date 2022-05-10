e8c551d0d5f0a4e5d363668cef6e9ee7ff09aacd
denev <denev723@gmail.com>
2022-05-10 13:44:25 +0900

    main banner 아래 box animation

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -57,7 +57,6 @@ const Box = styled(motion.div)<{ bgPhoto: string }>`
     background-size: cover;
     background-position: center center;
     height: 200px;
-    color: red;
     font-size: 66px;
 
     &:first-child {
@@ -66,6 +65,20 @@ const Box = styled(motion.div)<{ bgPhoto: string }>`
 
     &:last-child {
         transform-origin: center right;
+    }
+`;
+
+const Info = styled(motion.div)`
+    padding: 10px;
+    background-color: ${(props) => props.theme.black.lighter};
+    opacity: 0;
+    position: absolute;
+    width: 100%;
+    bottom: 0;
+
+    h4 {
+        text-align: center;
+        font-size: 18px;
     }
 `;
 
@@ -87,10 +100,21 @@ const boxVariants = {
     },
     hover: {
         scale: 1.3,
-        y: -50,
+        y: -80,
         transition: {
             delay: 0.5,
-            duration: 0.3,
+            duration: 0.1,
+            type: "tween"
+        }
+    }
+};
+
+const infoVariants = {
+    hover: {
+        opacity: 1,
+        transition: {
+            delay: 0.5,
+            duration: 0.1,
             type: "tween"
         }
     }
@@ -158,8 +182,11 @@ function Home() {
                                             variants={boxVariants}
                                             initial="normal"
                                             whileHover="hover"
-                                            transition={{ type: "tween" }}
-                                        />
+                                            transition={{ type: "tween" }}>
+                                            <Info variants={infoVariants}>
+                                                <h4>{movie.title}</h4>
+                                            </Info>
+                                        </Box>
                                     ))}
                             </Row>
                         </AnimatePresence>
